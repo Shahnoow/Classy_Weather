efc7cdf89cb84df3347cd764b6f3ac4ffffa6ea5
shahnoow <shahnawazjawed9@gmail.com>
2025-06-22 23:16:43 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -105,6 +105,10 @@ class App extends React.Component {
   render() {
     return (
       <div className="app">
+        <video autoPlay muted loop playsInline id="bg-video">
+          <source src="/weather.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
         <h1>Classy Weather</h1>
         <Input
           location={this.state.location}
